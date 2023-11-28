875d9ffbcbf19a5388595e8da722875091745b87
Pedro Noronha <pedro_noronha@outlook.com>
2023-11-28 16:57:05 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/xPedroNoronhax.png" alt="" />
+)
+
+export default Avatar
--- src/containers/SideBar/index.tsx
+++ src/containers/SideBar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/xPedroNoronhax.png" alt="" />
-    <Titulo fontSize={20}>Pedro Noronha</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Pedro Noronha</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        xPedroNoronhax
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
